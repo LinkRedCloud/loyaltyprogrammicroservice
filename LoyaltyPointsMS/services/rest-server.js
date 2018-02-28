1b5734c6e38fe4b1d0aeb73beffca294edcbd748
José Carlos Paiva Rodrigues <jose.rodrigues@linkconsulting.com>
2018-02-28 14:07:41 +0000

    Changes to work over ACCS

--- LoyaltyPointsMS/services/rest-server.js
+++ LoyaltyPointsMS/services/rest-server.js
@@ -12,6 +12,13 @@ function start (config, router) {
     
     app.use(bodyParser.json());
 
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    })
+
     app.use("/", router);
 
     var server = app.listen(config.server.port);
